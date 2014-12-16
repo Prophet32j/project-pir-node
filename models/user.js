060b70a6059f49434094fae8cb537233ccb62276
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-16 04:39:40 +0000

    Add getter, setter, and findById to user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
 // User Model for handling data layer
+
 // db is Mongo
 var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost/pir'),
+    users = db.get('users');
 
 /*
  * constructor for User Class
@@ -13,10 +17,34 @@ var User = function(data) {
 User.prototype.data = {}
 
 /*
+ * gets the attribute of this.data
+ * @param name of the field
+ * @return value of the named field
+ */
+User.prototype.get = function(name) {
+  return this.data[name];
+}
+
+/*
+ * sets the attribute of this.data
+ * @param name of the field
+ * @param value of the field
+ */
+User.prototype.set = function(name, value) {
+  this.data[name] = value;
+}
+
+/*
  * find a User by ID
- * @param id of the User
+ * @param id of the User as ObjectId or String
  * @param callback function(error, data)
  */
-User.prototype.findById = function(id, callback) {
-  
+User.findById = function(id, callback) {
+  users.findById(id, function(err, doc) {
+    if (err) return callback(err);
+    
+    callback(null, new User(doc));
+  });
 }
+             
+module.exports = User;
