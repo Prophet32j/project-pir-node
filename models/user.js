6bf25dcdd5ad6ef750a0bffe54206fcf51ecba85
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-16 04:04:46 +0000

    Add Monk dependency for ease of MongoDB usage

--- models/user.js
+++ models/user.js
@@ -1,4 +1,22 @@
 // User Model for handling data layer
+// db is Mongo
+var mongo = require('mongodb');
 
-var redis = require('redis');
-var client = redis.createClient();
+/*
+ * constructor for User Class
+ */
+var User = function(data) {
+  this.data = data;
+}
+
+// User data in JSON form
+User.prototype.data = {}
+
+/*
+ * find a User by ID
+ * @param id of the User
+ * @param callback function(error, data)
+ */
+User.prototype.findById = function(id, callback) {
+  
+}
