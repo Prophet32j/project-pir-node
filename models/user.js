bf41fc2d8bddaa7d0f4e2ad6b7c9a7921aecee87
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-18 05:16:48 +0000

    Add User model with basic db functions, tests for findById()

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 // User Model for handling data layer
 
-// db is Mongo
+// database is Mongo
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost/pir'),
+
+// getting users collection in Mongo
+var db = monk('localhost/test'),
     users = db.get('users');
 
 /*
@@ -35,13 +37,51 @@ User.prototype.set = function(name, value) {
 }
 
 /*
+ * saves the User data to the database
+ * @param callback function(error, user)
+ */
+User.prototype.save = function(callback) {
+  var self = this;
+  
+  // insert User if there is no Mongo ObjectId
+  if (!self.data._id)
+    return _insert(self, callback);
+  
+  // User is in collection, update
+  users.updateById(self.data._id, self.data, function(err) {
+    if (err) return callback(err);
+    
+    callback(null, self);
+  });
+}
+
+// private functions
+
+/*
+ * inserts User into users collection
+ * @param user to be inserted
+ * @param callback function(err, new User)
+ */
+function _insert(user, callback) {
+  users.insert(user.data, function(err, doc) {
+    if (err) return callback(err);
+    
+    callback(null, new User(doc));
+  });
+}
+
+// starting Class/namespace functions
+
+/*
  * find a User by ID
  * @param id of the User as ObjectId or String
- * @param callback function(error, data)
+ * @param callback function(error, User)
  */
 User.findById = function(id, callback) {
   users.findById(id, function(err, doc) {
     if (err) return callback(err);
+    
+    if (!doc) return callback(null, null);
     
     callback(null, new User(doc));
   });
--- tests/models/user.js
+++ tests/models/user.js
@@ -1,2 +1,54 @@
 // TODO
 // add some tests for the User model and its db functionality
+
+var app = require('./../../app');
+var expect = require('expect.js');
+var User = require('./../../models/user');
+
+describe('User Model', function() {
+  
+  describe('findById()', function() {
+    
+    it('should return null if no record found', function(done) {
+      User.findById('000000000000000000000000', function(err, user) {
+        expect(user).to.not.be.ok();
+        done();
+      });
+    });
+    
+    it('should return a User', function(done) {
+      User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+        expect(user).to.be.a(User);
+        done();
+      });
+    });
+    
+    it('should contain _id key in data', function(done) {
+      User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+        expect(user.data).to.have.key('_id');
+        done();
+      });
+    });
+  });
+  
+  describe.skip('find()', function() {
+    
+    it('should return an empty Array if collection is empty', function(done) {
+      
+    });
+    
+    it('should return an Array of User objects', function(done) {
+      
+    });
+    
+  });
+  
+  describe.skip('save()', function() {
+    
+    it('should save and return with ObjectId', function(done) {
+      
+    });
+    
+  });
+  
+});
