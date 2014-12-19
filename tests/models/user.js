f65de86827c041fead7478b8a4fcd63002f6b8b0
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-19 22:30:35 +0000

    Add more tests for better coverage

--- tests/models/user.js
+++ tests/models/user.js
@@ -2,90 +2,152 @@
 // add some tests for the User model and its db functionality
 
 var app = require('./../../app');
+
+// database is Mongo
+var mongo = require('mongodb');
+var monk = require('monk');
+// getting users collection in Mongo
+var db = monk('localhost/test'),
+    users = db.get('users');
+
 var expect = require('expect.js');
+
 var User = require('./../../models/user');
 
 describe('User Model', function() {
   
-  describe('findById()', function() {
+  describe('Finding Users in collection', function() {
+    var _id = users.id('5491b90e8e6a9e01d284a0d1');
+    var test_email = 'test.email@email.com';
+    var test_data = { _id: _id, username: 'test-user', email: test_email }
     
-    it('should return null if no record found', function(done) {
-      User.findById('000000000000000000000000', function(err, user) {
-        expect(user).to.not.be.ok();
-        done();
+    before('insert test user', function(done) {
+      
+      users.insert(test_data, function(err, doc) {
+        done(err);
       });
     });
     
-    it('should return a User', function(done) {
-      User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
-        expect(user).to.be.a(User);
-        done();
+    after('delete test user', function(done) {
+      users.remove({_id: _id}, function(err) {
+        done(err);
       });
     });
     
-    it('should contain _id key in data', function(done) {
-      User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
-        expect(user.data).to.have.key('_id');
-        done();
+    describe('findById()', function() {
+    
+      it('should return null if no record found', function(done) {
+        User.findById('000000000000000000000000', function(err, user) {
+          expect(user).to.not.be.ok();
+          done();
+        });
+      });
+
+      it('should return a User', function(done) {
+        User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+          expect(user).to.be.a(User);
+          done();
+        });
+      });
+
+      it('should contain _id key in data', function(done) {
+        User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+          expect(user.data).to.have.key('_id');
+          done();
+        });
       });
     });
+
+    describe('findByEmail()', function() {
+
+      it('should return null if no email is found', function(done) {
+        User.findByEmail('youshallnotpass@mail.com', function(err, user) {
+          expect(user).to.not.be.ok();
+          done();
+        });
+      });
+
+      it('should return a User when email is found', function(done) {
+        User.findByEmail(test_email, function(err, user) {
+          expect(user).to.be.a(User);
+          done();
+        });      
+      });
+
+      it('should return a User with an ObjectId', function(done) {
+        User.findByEmail(test_email, function(err, user) {
+          expect(user.data).to.have.key('_id');
+          done();
+        });
+      });
+    
+    });
+
+    describe('findAll()', function() {
+
+      it('should return an Array', function(done) {
+        User.findAll(function(err, users) {
+          expect(users).to.be.an(Array);
+          done();
+        });
+      });
+      
+      it('should contain at least 1 User', function(done) {
+        User.findAll(function(err, users) {
+          expect(users).to.not.be.empty();
+          done();
+        });
+      });
+
+    });
+
+    describe('find()', function() {
+
+      it('should return an Array', function(done) {
+        User.find({ username: test_data.username }, function(err, users) {
+          expect(users).to.be.an(Array);
+          done();
+        });
+      });
+      
+      it('should contain at least 1 User', function(done) {
+        User.findAll(function(err, users) {
+          expect(users).to.not.be.empty();
+          done();
+        });
+      });
+
+    });
+    
   });
   
-  describe('findByEmail()', function() {
-    
-    it('should return null if no email is found', function(done) {
-      User.findByEmail('youshallnotpass@mail.com', function(err, user) {
-        expect(user).to.not.be.ok();
-        done();
-      });
-    });
-    
-    it('should return a User when email is found', function(done) {
-      User.findByEmail('joshua.hardy@yahoo.com', function(err, user) {
-        expect(user).to.be.a(User);
-        done();
-      });      
-    });
-    
-    it('should return a User with an ObjectId', function(done) {
-      User.findByEmail('joshua.hardy@yahoo.com', function(err, user) {
-        expect(user.data).to.have.key('_id');
-        done();
-      });
-    });
-    
-  });
   
-  describe('findAll()', function() {
+  describe('Inserting and Modifying Records in Collection', function() {
     
-    it('should return an Array', function(done) {
-      User.findAll(function(err, users) {
-        expect(users).to.be.an(Array);
-        done();
+    describe('save()', function() {
+      var newUser = new User({ username: 'new-test-user', email: 'new_email@mail.com'});
+      
+      after('delete test user', function(done) {
+        users.remove({ username: newUser.username }, function(err) {
+          return done(err);
+        });
       });
-    });
-    
-  });
-  
-  describe('find()', function() {
-    
-    it('should return an Array', function(done) {
-      User.find({ username: 'new-user'}, function(err, users) {
-        expect(users).to.be.an(Array);
-        done();
+
+      it('should save new User and return with ObjectId', function(done) {
+        newUser.save(function(err, user) {
+          expect(user.data).to.have.key('_id');
+          done();
+        });
       });
-    });
-    
-  });
-  
-  describe('save()', function() {
-    
-    it('should save and return with ObjectId', function(done) {
-      var newUser = new User({ username: 'new-user', email: 'new_email@mail.com'});
-      newUser.save(function(err, user) {
-        expect(user.data).to.have.key('_id');
-        done();
+      
+      it('should return an empty error object when update is successful', function(done) {
+        newUser.email = 'changed_email@email.com';
+        newUser.save(function(err, user) {
+          expect(err).to.not.be.ok();
+          done();
+        });
       });
+
     });
     
   });
