55b45c7a52a8b5415cd30c359d23ba32a1ad14a1
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-20 22:20:35 +0000

    Add functions to try out API

--- public/js/client.js
+++ public/js/client.js
@@ -25,12 +25,33 @@ $(function() {
     });
   });
   
+  $('.a-users-del').click(function(event) {
+    var anchor = $(this);
+    var url = anchor.attr('href');
+    
+    event.preventDefault();
+    
+    $.ajax({
+      type: 'DELETE',
+      url: url
+    })
+    .done(function() {
+      anchor.remove();
+    })
+    .fail(function(message) {
+      alert(message);
+    });
+  });
+  
   function appendToList(users) {
     var list = [];
     users.forEach(function(user) {
-      var html = '<li>' + user.data.first_name + ' ' + user.data.last_name;
+      var html = '<li><a href="/users/' + user.data._id + '">' + 
+          user.data.first_name + ' ' + user.data.last_name + '</a>' + '\t' +
+          '<a class="a-users-del" href="/users/' + user.data._id + '">delete</a>';
       list.push(html);
     });
     $('#user-list').append(list);
   }
+  
 });
