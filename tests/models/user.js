770ce8125efc89b215f02658f5f0c0f8d19a09f8
Josh Hardy <josh.hardy.ufen@gmail.com>
2014-12-31 21:40:49 +0000

    comment out tests for user model and add tests for volunteer model

--- tests/models/user.js
+++ tests/models/user.js
@@ -1,154 +1,154 @@
-// var app = require('./../../app');
+// // var app = require('./../../app');
 
-// database is Mongo
-var monk = require('monk');
-// getting users collection in Mongo
-var db = monk('localhost/test'),
-    users = db.get('users');
+// // database is Mongo
+// var monk = require('monk');
+// // getting users collection in Mongo
+// var db = monk('localhost/test'),
+//     users = db.get('users');
 
-var expect = require('expect.js');
+// var expect = require('expect.js');
 
-var User = require('./../../models/user');
+// var User = require('./../../models/user');
 
-describe('User Model', function() {
+// describe('User Model', function() {
   
-  describe('Finding Users in collection', function() {
-    var _id = users.id('5491b90e8e6a9e01d284a0d1');
-    var test_email = 'test.email@email.com';
-    var test_data = { _id: _id, username: 'test-user', email: test_email }
+//   describe('Finding Users in collection', function() {
+//     var _id = users.id('5491b90e8e6a9e01d284a0d1');
+//     var test_email = 'test.email@email.com';
+//     var test_data = { _id: _id, username: 'test-user', email: test_email }
     
-    before('insert test user', function(done) {
-      users.insert(test_data, function(err, doc) {
-        done(err);
-      });
-    });
+//     before('insert test user', function(done) {
+//       users.insert(test_data, function(err, doc) {
+//         done(err);
+//       });
+//     });
     
-    after('delete test user', function(done) {
-      users.remove({_id: _id}, function(err) {
-        done(err);
-      });
-    });
+//     after('delete test user', function(done) {
+//       users.remove({_id: _id}, function(err) {
+//         done(err);
+//       });
+//     });
     
-    describe('findById()', function() {
+//     describe('findById()', function() {
     
-      it('should return null if no record found', function(done) {
-        User.findById('000000000000000000000000', function(err, user) {
-          expect(user).to.not.be.ok();
-          done();
-        });
-      });
+//       it('should return null if no record found', function(done) {
+//         User.findById('000000000000000000000000', function(err, user) {
+//           expect(user).to.not.be.ok();
+//           done();
+//         });
+//       });
 
-      it('should return a User', function(done) {
-        User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
-          expect(user).to.be.a(User);
-          done();
-        });
-      });
+//       it('should return a User', function(done) {
+//         User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+//           expect(user).to.be.a(User);
+//           done();
+//         });
+//       });
 
-      it('should contain _id key in data', function(done) {
-        User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
-          expect(user.data).to.have.key('_id');
-          done();
-        });
-      });
-    });
+//       it('should contain _id key in data', function(done) {
+//         User.findById('5491b90e8e6a9e01d284a0d1', function(err, user) {
+//           expect(user.data).to.have.key('_id');
+//           done();
+//         });
+//       });
+//     });
 
-    describe('findByEmail()', function() {
+//     describe('findByEmail()', function() {
 
-      it('should return null if no email is found', function(done) {
-        User.findByEmail('youshallnotpass@mail.com', function(err, user) {
-          expect(user).to.not.be.ok();
-          done();
-        });
-      });
+//       it('should return null if no email is found', function(done) {
+//         User.findByEmail('youshallnotpass@mail.com', function(err, user) {
+//           expect(user).to.not.be.ok();
+//           done();
+//         });
+//       });
 
-      it('should return a User when email is found', function(done) {
-        User.findByEmail(test_email, function(err, user) {
-          expect(user).to.be.a(User);
-          done();
-        });      
-      });
+//       it('should return a User when email is found', function(done) {
+//         User.findByEmail(test_email, function(err, user) {
+//           expect(user).to.be.a(User);
+//           done();
+//         });      
+//       });
 
-      it('should return a User with an ObjectId', function(done) {
-        User.findByEmail(test_email, function(err, user) {
-          expect(user.data).to.have.key('_id');
-          done();
-        });
-      });
+//       it('should return a User with an ObjectId', function(done) {
+//         User.findByEmail(test_email, function(err, user) {
+//           expect(user.data).to.have.key('_id');
+//           done();
+//         });
+//       });
     
-    });
+//     });
 
-    describe('findAll()', function() {
+//     describe('findAll()', function() {
 
-      it('should return an Array', function(done) {
-        User.findAll(function(err, users) {
-          expect(users).to.be.an(Array);
-          done();
-        });
-      });
+//       it('should return an Array', function(done) {
+//         User.findAll(function(err, users) {
+//           expect(users).to.be.an(Array);
+//           done();
+//         });
+//       });
       
-      it('should contain at least 1 User', function(done) {
-        User.findAll(function(err, users) {
-          expect(users).to.not.be.empty();
-          done();
-        });
-      });
+//       it('should contain at least 1 User', function(done) {
+//         User.findAll(function(err, users) {
+//           expect(users).to.not.be.empty();
+//           done();
+//         });
+//       });
 
-    });
+//     });
 
-    describe('find()', function() {
+//     describe('find()', function() {
 
-      it('should return an Array', function(done) {
-        User.find({ username: test_data.username }, function(err, users) {
-          expect(users).to.be.an(Array);
-          done();
-        });
-      });
+//       it('should return an Array', function(done) {
+//         User.find({ username: test_data.username }, function(err, users) {
+//           expect(users).to.be.an(Array);
+//           done();
+//         });
+//       });
       
-      it('should contain at least 1 User', function(done) {
-        User.find({ username: test_data.username }, function(err, users) {
-          expect(users).to.not.be.empty();
-          done();
-        });
-      });
+//       it('should contain at least 1 User', function(done) {
+//         User.find({ username: test_data.username }, function(err, users) {
+//           expect(users).to.not.be.empty();
+//           done();
+//         });
+//       });
 
-    });
+//     });
     
-  });
+//   });
   
   
-  describe('Inserting and Modifying Records in Collection', function() {
+//   describe('Inserting and Modifying Records in Collection', function() {
     
-    describe('save()', function() {
-      var newUser = new User({ username: 'new-test-user', email: 'new_email@mail.com'});
+//     describe('save()', function() {
+//       var newUser = new User({ username: 'new-test-user', email: 'new_email@mail.com'});
       
-      after('delete test user', function(done) {
-        users.remove({ username: newUser.data.username }, function(err) {
-          done(err);
-        });
-      });
+//       after('delete test user', function(done) {
+//         users.remove({ username: newUser.data.username }, function(err) {
+//           done(err);
+//         });
+//       });
 
-      it('should save new User and return with ObjectId', function(done) {
-        newUser.save(function(err, user) {
-          newUser = user;
-          expect(user.data).to.have.key('_id');
-          done();
-        });
-      });
+//       it('should save new User and return with ObjectId', function(done) {
+//         newUser.save(function(err, user) {
+//           newUser = user;
+//           expect(user.data).to.have.key('_id');
+//           done();
+//         });
+//       });
       
-      it('should return an empty error object when update is successful', function(done) {
-        users.findOne({username: newUser.data.username}, function(err, doc) {
-          newUser.data = doc;
-          newUser.set('email', 'changed_email@email.com');
-          newUser.save(function(err, user) {
-            expect(err).to.not.be.ok();
-            done();
-          });
-        });
-      });
+//       it('should return an empty error object when update is successful', function(done) {
+//         users.findOne({username: newUser.data.username}, function(err, doc) {
+//           newUser.data = doc;
+//           newUser.set('email', 'changed_email@email.com');
+//           newUser.save(function(err, user) {
+//             expect(err).to.not.be.ok();
+//             done();
+//           });
+//         });
+//       });
 
-    });
+//     });
     
-  });
+//   });
   
-});
+// });
