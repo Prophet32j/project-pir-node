b102927a99ff1636e5bdf71e023482464ecd6a45
Josh Hardy <josh.hardy.ufen@gmail.com>
2015-01-18 03:15:45 +0000

    delete schemas.js

--- models/schemas.js
+++
@@ -1,8 +0,0 @@
-// use this to init all models in Mongoose
-var models = ['admin', 'front-desk', 'parent', 'reader', 'volunteer'];
-
-exports.init = function() {
-  models.forEach(function(model) {
-    require('./' + model)();
-  });
-}
