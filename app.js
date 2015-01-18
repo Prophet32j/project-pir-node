30f1c70e0cfea3de07452cc8f8ff31a6ccc9cf8e
Josh Hardy <josh.hardy.ufen@gmail.com>
2015-01-18 03:14:04 +0000

    clean up express app to deploy upon db init

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-// set up Mongo and Mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var config = require('./config/config.json');
+
+mongoose.connect('mongodb://localhost/' + config.db.name);
 
 // make sure everything connected correctly
 var db = mongoose.connection;
@@ -11,27 +12,28 @@ db.on('error', function(err) {
   console.log('Error: ' + err);
   process.exit();
 });
+
 db.once('open', function() {
   console.log('Connection open');
+  
+  // initialize all mongoose models
+  require('./models')();
+  
+  // static file serving
+  app.use(express.static(__dirname + '/public'));
+
+  // routes
+  var parentRoutes = require('./routes/parents');
+  app.use('/parents', parentRoutes);
+
+  var readerRoutes = require('./routes/readers');
+  app.use('/readers', readerRoutes);
+
+  var volunteerRoutes = require('./routes/volunteers');
+  app.use('/volunteers', volunteerRoutes);
+
+  var pairRoutes = require('./routes/pairs');
+  app.use('/pairs', pairRoutes);
 });
-
-// initialize all mongoose models
-require('./models/schemas').init();
-
-// static file serving
-app.use(express.static(__dirname + '/public'));
-
-// routes
-var parentRoutes = require('./routes/parents');
-app.use('/parents', parentRoutes);
-
-var readerRoutes = require('./routes/readers');
-app.use('/readers', readerRoutes);
-
-var volunteerRoutes = require('./routes/volunteers');
-app.use('/volunteers', volunteerRoutes);
-
-var pairRoutes = require('./routes/pairs');
-app.use('/pairs', pairRoutes);
 
 module.exports = app;
