9d407889e24baeee68bfc5e34e35fb9b9c7950c2
Josh Hardy <josh.hardy.ufen@gmail.com>
2015-01-31 05:10:09 +0000

    add JWT authentication and consolidate parsing middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
+var bodyParser = require('body-parser')
+var authjwt = require('./auth/auth-jwt');
 
 var mongoose = require('mongoose');
 var config = require('./config/config.json');
 
-mongoose.connect(process.env.MONGOLAB_URI || config.db.uri);
+mongoose.connect(process.env.MONGOLAB_URI || config.mongodb.url);
 
 // make sure everything connected correctly
 var db = mongoose.connection;
@@ -22,21 +24,35 @@ db.once('open', function() {
   // static file serving
   app.use(express.static(__dirname + '/public'));
 
-  // routes
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  
+  // non-REST utility routes
+  var index = require('./routes');
+  app.post('/login', index.login);
+  app.get('/logout', index.logout);
+  app.post('/register', index.register);
+
+
+  // application routes
+  // secure all API routes with tokens
+  app.use('/api', authjwt(config.jwt_secret));
+
   var userRoutes = require('./routes/users');
-  app.use('/users', userRoutes);
+  app.use('/api/users', userRoutes);
   
   var parentRoutes = require('./routes/parents');
-  app.use('/parents', parentRoutes);
+  app.use('/api/parents', parentRoutes);
 
   var readerRoutes = require('./routes/readers');
-  app.use('/readers', readerRoutes);
+  app.use('/api/readers', readerRoutes);
 
   var volunteerRoutes = require('./routes/volunteers');
-  app.use('/volunteers', volunteerRoutes);
+  app.use('/api/volunteers', volunteerRoutes);
 
   var pairRoutes = require('./routes/pairs');
-  app.use('/pairs', pairRoutes);
+  app.use('/api/pairs', pairRoutes);
 });
 
 module.exports = app;
