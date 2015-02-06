e47419d1ab46d29cd480af8d208c62853c47543b
Josh Hardy <josh.hardy.ufen@gmail.com>
2015-02-06 05:12:14 +0000

    make mailer into object as first round of modularizing, add templates

--- mailer/index.js
+++ mailer/index.js
@@ -4,6 +4,41 @@ var client = new mandrill.Mandrill(config.mandrill_api_key);
 var Handlebars = require('handlebars');
 var fs = require('fs');
 var path = require('path');
+var email_config = require('./config.json');
+
+var Mailer = function() { }
+
+/*
+ * compiles raw handlebars and data into usable html
+ * @param source to compile
+ * @param data to insert
+ * @return html
+ */
+Mailer.prototype.compile = function(source, data) {
+  var template = Handlebars.compile(source);
+  var html = template(data);
+
+  return html;
+}
+
+Mailer.prototype.loadTemplate = function(templateName, callback) {
+  var filePath = path.resolve('mailer/templates', './' + templateName + '.hbs');
+  fs.readFile(filePath, { encoding: 'utf-8' }, callback);
+}
+
+Mailer.prototype.loadTemplateSync = function(templateName) {
+  var filePath = path.resolve('mailer/templates', './' + templateName);
+  return fs.readFileSync(filePath, { encoding: 'utf-8' });
+}
+
+Mailer.prototype.loadTemplateAndCompile = function(templateName, data, callback) {
+  this.loadTemplate(templateName, function(err, hbs) {
+    if (err)
+      return callback(err);
+
+    callback(null, this.compile(hbs, data));
+  });
+}
 
 /*
  * sends a single email to a recipient
@@ -13,7 +48,10 @@ var path = require('path');
  * @param html of the email
  * @param callback function(error, emails[])
  */
-exports.sendEmail = function(to, from, subject, html, callback) {
+Mailer.prototype.sendEmail = function(to, from, subject, html, callback) {
+  if (!from) {
+    from = email_config.system_email;
+  }
   var message = {
     to: to,
     html: html,
@@ -30,20 +68,4 @@ exports.sendEmail = function(to, from, subject, html, callback) {
   });
 }
 
-/*
- * compiles raw handlebars and data into usable html
- * @param source to compile
- * @param data to insert
- * @return html
- */
-exports.compileHbs = function(source, data) {
-  var template = Handlebars.compile(source);
-  var html = template(data);
-
-  return html;
-}
-
-exports.getTemplate = function(templateName, callback) {
-  var filePath = path.resolve('mailer/templates', './' + templateName);
-  fs.readFile(filePath, { encoding: 'utf-8' }, callback);
-}
+module.exports = Mailer;
