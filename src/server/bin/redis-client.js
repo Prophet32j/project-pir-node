9dafdd0ae54ac1ea6bacf002864a2ed1e20d454e
Joshua Hardy <josh.hardy.ufen@gmail.com>
2015-05-19 15:37:47 -0500

    readd redis db for session store

--- src/server/bin/redis-client.js
+++ src/server/bin/redis-client.js
@@ -1,16 +1,16 @@
-// var redis = require('redis');
-// var config = require('./../config/config')
+var redis = require('redis');
+var config = require('./../config/config')
 
-// module.exports = function() {
-//   var client;
-//   if (process.env.REDISTOGO_URL) {
-//     var rtg = require('url').parse(process.env.REDISTOGO_URL);
-//     client = redis.createClient(rtg.port, rtg.hostname);
-//     client.auth(rtg.auth.split(':')[1]);
-//   } else {
-//     var rtg = require('url').parse(config.redistogo.url);
-//     client = redis.createClient(rtg.port, rtg.hostname);
-//     client.auth(rtg.auth.split(':')[1]);
-//   }
-//   return client;
-// }
+module.exports = function() {
+  var client;
+  if (process.env.REDISTOGO_URL) {
+    var rtg = require('url').parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(':')[1]);
+  } else {
+    var rtg = require('url').parse(config.redistogo.url);
+    client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(':')[1]);
+  }
+  return client;
+}
